24a0fd7622c38d0790aff001637916c7e8a14137
Dianelys Rocha <45056481+JiRodya@users.noreply.github.com>
2025-04-25 23:03:47 -0400

    Added write/read bit and byte

--- core/inc/onewire.h
+++ core/inc/onewire.h
@@ -6,18 +6,23 @@
 #include <cmath>            // for NAN
 #include <vector>
 
-#define ONEWIRE(pin) (*OneWire::getInstance(pin))
-#define ONEWIRE_WITH_PIN(pin,rom_id) (*OneWire::getInstance(pin,rom_id))
+#define ONEWIRE(pin) (*OneWire::Get(pin))
+#define ONEWIRE_WITH_PIN(pin,rom_id) (*OneWire::Get(pin,rom_id))
 
 
 class OneWire {
 
 public:
-    static OneWire* getInstance(gpio_num_t pin);
-    static OneWire* getInstance(gpio_num_t pin, uint64_t rom_id);
+    static OneWire* Get(gpio_num_t pin);
+    static OneWire* Get(gpio_num_t pin, uint64_t rom_id);
 
     gpio_num_t getPin() const;
     uint64_t getROM() const;
+
+    void writeBit (bool bit);
+    void writeByte(uint8_t byte);
+    uint8_t readBit();
+    uint8_t readByte();
 private:
     gpio_num_t _pin;
     uint64_t _rom = 0;
--- core/src/onewire.cpp
+++ core/src/onewire.cpp
@@ -1,7 +1,12 @@
 #include "onewire.h"
 #include "esp_log.h"
 
-OneWire* OneWire::getInstance(gpio_num_t pin, uint64_t rom_id) {
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+
+OneWire* OneWire::Get(gpio_num_t pin, uint64_t rom_id) {
     // If the exact (pin + rom) instance exists, reuse it
     for (auto* inst : _instances) {
         if (inst->_pin == pin && inst->_rom == rom_id)
@@ -15,7 +20,7 @@ OneWire* OneWire::getInstance(gpio_num_t pin, uint64_t rom_id) {
 }
 
 
-OneWire* OneWire::getInstance(gpio_num_t pin) {
+OneWire* OneWire::Get(gpio_num_t pin) {
     for (auto* inst : _instances) {
         if (inst->_pin == pin) {
             if (inst->_rom == 0) return inst;  // Exact match
@@ -30,3 +35,54 @@ OneWire* OneWire::getInstance(gpio_num_t pin) {
     return newInst;
 }
 
+void OneWire::writeBit(bool bit) {
+    gpio_set_direction(_pin, GPIO_MODE_OUTPUT);
+
+    if (bit) {
+        // Write '1' bit
+        gpio_set_level(_pin, 0);
+        esp_rom_delay_us(6);      // Pull low for 6 us
+        gpio_set_level(_pin, 1);
+        esp_rom_delay_us(64);     // Release line and wait for rest of the timeslot
+    } else {
+        // Write '0' bit
+        gpio_set_level(_pin, 0);
+        esp_rom_delay_us(60);     // Pull low for 60 us
+        gpio_set_level(_pin, 1);
+        esp_rom_delay_us(10);     // Release line
+    }
+}
+
+uint8_t OneWire::readBit(){
+    gpio_set_direction(_pin, GPIO_MODE_OUTPUT);
+
+    // Write '0' bit for 1-15 us to start the read
+    gpio_set_level(_pin, 0);
+    esp_rom_delay_us(6);
+
+    gpio_set_direction(_pin, GPIO_MODE_INPUT);
+    // slave sending '1' -> do nothing, bus goes to pull-up
+    // slave sending '0' -> bus goes low for 60 us
+    esp_rom_delay_us(15); // wait 15 us
+    uint8_t bit = gpio_get_level(_pin);
+    esp_rom_delay_us(45); // wait 45 us
+    return bit;
+}
+
+void OneWire::writeByte(uint8_t byte) {
+    for (int i = 0; i < 8; i++) {
+        bool bit = byte & 0x01;    // Get the least significant bit (LSB)
+        writeBit(bit);             // Write that bit
+        byte >>= 1;                // Shift right to get the next bit
+    }
+}
+
+uint8_t OneWire::readByte() {
+    uint8_t value = 0;
+    for (int i = 0; i < 8; i++) {
+        if (readBit()) {
+            value |= (1 << i); // Set the i-th bit if readBit() returned 1
+        }
+    }
+    return value;
+}
